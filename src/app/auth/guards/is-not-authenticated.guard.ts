8094670f2c66bfa9730d572b27b300fb22206ef4
Irvin Fernandez <ifernandez.estevezjor@gmail.com>
2024-07-31 18:05:50 -0600

    Functional Guards, diseño

---
+++ src/app/auth/guards/is-not-authenticated.guard.ts
@@ -0,0 +1,22 @@
+import { Router, CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+import { AuthStatus } from '../interfaces';
+
+export const isNotAuthenticatedGuard: CanActivateFn = (route, state) => {
+
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    if(authService.authStatus() === AuthStatus.authenticated){
+        return true;
+    }
+
+    if(authService.authStatus() === AuthStatus.checking){
+        return false;
+    }
+
+    router.navigateByUrl('/auth/login')
+    return true;
+
+};
---
+++ src/app/auth/guards/isAuthenticated.guard.ts
@@ -0,0 +1,24 @@
+import { Router, CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+import { AuthStatus } from '../interfaces';
+
+export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
+
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if(authService.authStatus() === AuthStatus.authenticated){
+    return true;
+  }
+
+  if(authService.authStatus() === AuthStatus.checking){
+    return false;
+  }
+
+  router.navigateByUrl('/auth/login')
+  return false;
+
+  /* console.log('isAuthenticatedGuard');
+  console.log({route, state}); */
+};
